f71b0aa728a53f0208184adbf737ee9a5bfa0995
DevaKrishna07 <devakrishna1418@gmail.com>
2025-11-26 15:39:49 +0530

    shadcomponent added

---
+++ src/components/common/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { DottedGlowBackground } from '../ui/dotted-glow-background'
+
+function HeroSection() {
+  return (
+    <div className='w-screen min-h-screen'>
+    herosection
+    </div>
+  )
+}
+
+export default HeroSection
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HeroSection from '../components/common/HeroSection'
 
 const Home = () => {
   return (
-    <div>
-      homepage
+    <div className='flex w-screen min-h-screen items-center justify-center flex-col overflow-x-hidden bg-black'>
+      <HeroSection />
     </div>
   )
 }
