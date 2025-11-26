b00f1b10468e7f80aaf8380618d98185ec5a47dc
DevaKrishna07 <devakrishna1418@gmail.com>
2025-11-26 20:33:02 +0530

    error rectified

--- src/components/common/HeroSection.jsx
+++ src/components/common/HeroSection.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { BackgroundGradientAnimation } from '../ui/background-gradient-animation';
 import logo from '../../assets/logo.png';
+import HoverBtn from './HoverBtn';
 
 function HeroSection() {
   return (
    <BackgroundGradientAnimation>
       <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
         <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-b from-white/80 to-white/20">
-          <h1 className='text-8xl'>AI-For-Everyone</h1>
+          <h1 className='text-8xl mb-5'>AI-For-Everyone</h1>
           <h2 className='text-2xl'>Join a movement making AI education accessible to students, elders, professionals, and communities.<br/> Learn, explore, and engage responsibly.</h2>
+          <HoverBtn />
         </p>
       </div>
     </BackgroundGradientAnimation>
---
+++ src/components/common/HoverBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HoverBtn() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HoverBtn
