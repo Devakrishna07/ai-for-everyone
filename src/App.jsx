51bb821becf72306bff59568db7f4f4973466cd8
DevaKrishna07 <devakrishna1418@gmail.com>
2025-11-28 12:33:31 +0530

    Scrollbar removed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home'
 export default function App() {
   return (
     <BrowserRouter>
-       <div className='overflow-x-hidden'>
+       <div className='overflow-x-hidden hide-scrollbar '>
         <Routes>
         <Route path='/' element={<Home />} />
        </Routes>
--- src/layouts/OurApproach.jsx
+++ src/layouts/OurApproach.jsx
@@ -12,7 +12,7 @@ const statsData = [
 
 function OurApproach() {
   return (
-    <div className='w-full flex flex-col items-center justify-center bg-gradient-to-br from-blue-900/40 to-black p-4'>
+    <div className='w-full flex flex-col items-center justify-center bg-gradient-to-br from-blue-900/40 to-black p-8'>
         <h1 className='text-white/70 text-6xl sm:text-7xl font-semibold my-3 '>Our Approach </h1>
         <span className='text-white/60 text-2xl m-2 mb-6 font-Ubuntulight'>A holistic framework for AI literacy</span><br />
         <CardGrid data={statsData} count={3} />
